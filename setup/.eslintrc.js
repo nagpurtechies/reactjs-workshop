f43befa22fb75c6e126eed5e964ecaaa8dbc6430
Tushar Joshi <tusharvjoshi@gmail.com>
2017-07-23 00:20:57 +0530

    Add linebreak rule for windows

--- setup/.eslintrc.js
+++ setup/.eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
     "rules": {
         "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
         "import/no-extraneous-dependencies": [2, { devDependencies: true }],
+        "linebreak-style": ["error", "windows"]
     },
 };
