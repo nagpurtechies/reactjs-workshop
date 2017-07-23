bb0582c56c26d9c0266f34c64a5b0f8cfb43f43e
Tushar Joshi <tusharvjoshi@gmail.com>
2017-07-23 05:37:23 +0530

    Add Todo component

---
+++ lab11/finish/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "import/no-extraneous-dependencies": [2, { devDependencies: true }],
+        "linebreak-style": ["error", "windows"]
+    },
+};
---
+++ lab11/finish/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AddTodo from './components/addTodo/';
+
+const App = () => (
+  <div>
+    <h1>Todo list</h1>
+    <AddTodo submitTodo={() => {}} />
+  </div>
+);
+
+export default App;
